82afc5d860f95be79e0065e4b9c9b54bd35f7fec
rosoporto <rosoporto@yandex.ru>
2023-02-21 23:35:49 +0300

    Поменял описание функциям

--- modules/get_extension.py
+++ modules/get_extension.py
@@ -3,14 +3,14 @@ from urllib.parse import urlparse
 
 
 def get_extension(url):
+    """
+    Функция возвращает расширение файла по ссылке
+    """
     path_file = urlparse(url).path
     return os.path.splitext(path_file)[-1]
 
 
 def main():
-    """
-    Функция возвращает расширение файла по ссылке
-    """
     url = "https://example.com/txt/hello%20world.txt?v=9#python"
     print(get_extension(url))
 
