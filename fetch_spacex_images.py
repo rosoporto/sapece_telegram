468b3cb7ec05e862687556f5cd112fab54ab7616
rosoporto <rosoporto@yandex.ru>
2023-03-29 22:32:33 +0300

    оптимизировал функцию. Переделал логику

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -1,9 +1,12 @@
 from modules.download import get, download
 
 
-def fetch_spacex_last_launch(foto_path, prefix="spacex", id="5eb87d47ffd86e000604b38a"):
-    url = "https://api.spacexdata.com/v5/launches/" + id
-    r = get(url).json()
+def fetch_spacex_last_launch(foto_path, prefix, id="5eb87d47ffd86e000604b38a"):
+    url = "https://api.spacexdata.com/v5/launches/"
+    r = get(url).json()[-1]
+    if not r["links"]["flickr"]["original"]:
+        url = "https://api.spacexdata.com/v5/launches/" + id
+        r = get(url).json()
     photos = r["links"]["flickr"]["original"]
     for index, photos in enumerate(photos):
         photo_name = prefix + str(index) + ".jpg"
