f34a9be98afab5e8de79d9d2ede03543d72f0af7
rosoporto <rosoporto@yandex.ru>
2023-03-29 22:29:48 +0300

    изменил функцию на работу с одним файлом фото

--- telegram_bot.py
+++ telegram_bot.py
@@ -13,13 +13,11 @@ def get_chat_id(token):
         print("Set Start in you're Bot")
 
 
-def send_photo_telegram(token, name_folder_photos):
-        bot = telegram.Bot(token)
-        photos = getImage(name_folder_photos)
-        chat_id = get_chat_id(token)
-        for photo in photos:
-            bot.send_photo(chat_id=chat_id, photo=open(photo, 'rb'))
-        return "Complate!"
+def send_photo_telegram(token, photo):
+    bot = telegram.Bot(token)
+    chat_id = get_chat_id(token)
+    bot.send_photo(chat_id=chat_id, photo=open(photo, 'rb'))
+    return "Complate!"
 
 
 def main():
